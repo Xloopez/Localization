467e9e74c71d55c7fc4e7edb5e426bbeb3b63fc9
nicolelopezfeliz <nicole.lopez@live.se>
2020-09-16 10:46:17 +0200

    Translated mainActivity layout to swedish

--- app/src/main/java/com/example/loc/MainActivity.kt
+++ app/src/main/java/com/example/loc/MainActivity.kt
@@ -2,17 +2,38 @@ package com.example.loc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var greetingTextView: TextView
+    var name = "David"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        greetingTextView.findViewById<TextView>(R.id.textView)
+
     }
+
+
+
+    fun button_pressed(view: View){
+        greetingTextView.text = getString(R.string.great, name) //Den skriver ut angivna strängen i strings.xml plus variablen namnet namn
+    }
+
+    //På engelska vill vi att det skall stå - Great David!
+    //På svenska vill vi att det skall stå - Bra jobbat David!!!
+
 }
 
 
 
 //Anpassa appen till svenska och engelska.
 //1. anpassa xml - layout        x
-//2. anpassa vår kotlin fil
+//2. anpassa vår kotlin fil      x
 //3. anpassa bilder
